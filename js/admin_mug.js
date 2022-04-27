f77804fc8addee618ce4697e0da27a6ac50f56fc
Robert Nelson <robert-git@nelson.house>
2022-04-27 12:09:10 -0700

    Revert "Merge branch 'working' of github.com:Robert-Nelson/MugShot into working"
    This reverts commit 4f103c068167c799535402e4a4ebb917eedb3b31, reversing
    changes made to 03ec578b75129479859dd77236299e9f6527862c.

--- js/admin_mug.js
+++ js/admin_mug.js
@@ -348,8 +348,6 @@ var MugShot = {
     name.style.top = parseInt(mug.top) + parseInt(mug.height) + 'px';
     name.style.left = mug.el.style.left;
     name.style.width = mug.el.style.width;
-    name.autocomplete = false;
-    name.type = "text";
     document.getElementById(this.id2).append(name);
     this.mugs[this.cfi].name.el = name;
     this.mugs[this.cfi].frame.el.title = mug.name != null ? mug.name : 'Unidentified Person';
